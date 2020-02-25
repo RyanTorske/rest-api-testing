72e668efa9116aaa253891dd9b264df577731ca0
Ryan Torske <rtorske@cnm.edu>
2020-02-25 16:13:46 -0700

    Started favorite API section.

---
+++ favorite/favorite.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/Classes/autoload.php";
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
+require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/lib/jwt.php";
+require_once dirname(__DIR__, 3) . "/lib/uuid.php";
+
+use SuperSmashLore\SuperSmashLore\Profile;
+/**
+ * API for the favorite class
+ */
+
+//verify the session, start if not already active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare and empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	
+}
