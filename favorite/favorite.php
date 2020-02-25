44f214e90fbb9433217f8116f63ef149d36e2f2f
Ryan Torske <rtorske@cnm.edu>
2020-02-25 16:30:12 -0700

    Making progress on favorite API.

--- favorite/favorite.php
+++ favorite/favorite.php
@@ -23,5 +23,37 @@ $reply->status = 200;
 $reply->data = null;
 
 try {
-	
+	$secrets = new \Secrets("/etc/apache2/capstone-mysql/smash.ini");
+	$pdo = $secrets->getPdoObject();
+
+	//determine which HTTP method was used
+	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
+
+	//sanitize the searching param
+	$favoriteProfileId = $id = filter_input(INPUT_GET, "favoriteProfileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$favoriteCharacterId = $id = filter_input(INPUT_GET, "favoriteCharacterId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+	if($method === "GET") {
+		//set xsrf cookie
+		setXsrfCookie();
+
+		//gets a specific favorite associated based on the composite key
+		if ($favoriteProfileId !== null && $favoriteCharacterId !== null) {
+			$favorite = Favorite::getFavoriteByFavoriteProfileIdAndFavoriteCharacterId ($pdo, $favoriteProfileId, $favoriteCharacterId);
+
+			if($favorite !== null) {
+				$reply->data = $favorite;
+			}
+			//if none of the search parameters are met, throw and exception
+		} else if(empty($favoriteProfileId) === false) {
+			$reply->data = Favorite::getFavoriteByFavoriteProfileId($pdo, $favoriteProfileId)->toArray();
+			//get all the favorites associated with the characterId
+		} else if(empty($favoriteCharacterId) == false) {
+			$reply->data = Favorite::getFavoriteByFavoriteChaaracterId($pdo, $favoriteCharacterId)->toArray();
+		} else {
+			throw new InvalidArgumentException("Incorrect Search Parameters", 404);
+		}
+	} else if($method === "POST" || $method === "PUT") {
+		//decode the response from the front end
+	}
 }
